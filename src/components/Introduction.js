0589d77c78b81a2eed30e966e0bf9174020f3e8a
Dorian <silisyumtr@gmail.com>
2021-08-13 09:52:44 +0300

    add Contact Card

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,6 +2,7 @@ import {Typography,Grid,Avatar } from '@material-ui/core'
 import React from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import PortfilioCard from './PortfilioCard';
+import ContactCard from './ContactCard';
 
 const useStyles = makeStyles({
 
@@ -11,9 +12,7 @@ height:'fit-content',
 position:'relative',
 minHeight:'100vh',
     },
-    box: {
-    
-    },
+   
     header:{
 color:'#D8B384',
 paddingTop:'40px',
@@ -52,21 +51,18 @@ const Introduction =()=>{
 
 return(
     <div className={classes.container}>
-         <div className={classes.box}>
-        <Avatar alt={'Avatar'} className={classes.avatar} src={'https://avatars.githubusercontent.com/u/71990861?v=4'}></Avatar>
-
-        </div>
-<Typography className={classes.header} align='center' variant='h3'> I AM A FULL-STACK DEVELOPER</Typography>
-
        
-        <Typography align='center'className={classes.text}>
+  
+
+<Grid justify={'center'}className={classes.portfilioContainer}container spacing={0}>
+<ContactCard/>
+<Typography align='center'className={classes.text}>
         I am comfortable in both front-end and back-end technologies, such as; React, HTML, CSS, Material UI, MongoDb, REST and Graph QL api's
         as well as confident in javascript and it's common libraries 
 
 
 </Typography>
 
-<Grid justify={'center'}className={classes.portfilioContainer}container spacing={0}>
 {[
     {
      projectTitle:'Anime Search Bar',
